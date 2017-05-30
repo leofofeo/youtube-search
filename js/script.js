e56789a993fc11a0e77f3c633db0669f148df2bb
Leo Rubiano <leo.a.rubiano@gmail.com>
2017-05-30 11:22:28 -0400

    testing results from search api

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
 //JS and jQuery for RQ
 $('document').ready(function(){
-
+	console.log('document ready');
 
 });
-
 
 $('#query').on('focus', function(){
 	$(this).animate({
@@ -20,9 +19,34 @@ $('#query').on('blur', function(){
 		$('#query').animate({
 			width:'45%'
 		}, 800, function(){});
-		// $('#search-btn').animate({
-		// 	right: '360px'
-		// }, 400, function(){});
 	}
 
 });
+
+
+$('#search-form').submit(function(e){
+	e.preventDefault();
+	searchYouTube();
+});
+
+
+var searchYouTube = function(){
+	$('#results').html('');
+	$('#buttons').html('');
+	var query = $('#query').val();
+
+	$.get(
+		'https://www.googleapis.com/youtube/v3/search', {
+		part: 'snippet, id',
+		q: query,
+		type: 'video',
+		key: 'AIzaSyB-WogP-SMI3M6WKTijzpWmpRvZAbWg6Xg'},
+		function(data){
+			console.log('from internal get()');
+			var nextPageToken = data.nextPageToken;
+			var prevPageToken = data.prevPageToken;
+			console.log(data);
+
+		}
+	);
+}
